8078333d5b7f49844ea4195b48dd276d3a4cd76e
Wloner0809 <wloner@mail.ustc.edu.cn>
2025-06-29 15:02:20 +0000

    feat(eval): save incorrect responses

---
+++ evaluation/eval-Qwen2.5_Math/scripts/save_verifier_incorrect.sh
@@ -0,0 +1,5 @@
+python save_verifier_incorrect.py \
+    --test_set_path /mnt/blob/Mirage/search/Llama-bug-fixed/Skywork-PRM-vllm0.6.4.post1/100test/llama3.1_8B_Instruct-math100-dvts_256answers_per_question-sorted.jsonl \
+    --benchmark_name /mnt/blob/Mirage/benchmark/MATH100.jsonl \
+    --prm_reward_path /mnt/blob/Mirage/search/Llama-bug-fixed/Skywork-PRM-vllm0.6.4.post1/100test/llama3.1_8B_Instruct-math100-dvts_256answers_per_question-sorted.jsonl \
+    --output_path /mnt/blob/Mirage/search/Llama-results/100test/math100/llama3.1_8B
